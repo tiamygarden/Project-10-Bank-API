d5b224d9e48813fdc441123ec96769c5e8214b80
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-06-08 16:47:58 +0200

    install redux et router

---
+++ client/src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "../pages/Home"
+import Profil from "../pages/Profil"
+import Reglage from "../pages/Reglage"
+import Communaute from "../pages/Communaute"
+import Error404 from "../pages/Error404"
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profil/user/:userId" element={<Profil />} />
+        <Route path="/reglage" element={<Reglage />} />
+        <Route path="/communaute" element={<Communaute />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
+ 
