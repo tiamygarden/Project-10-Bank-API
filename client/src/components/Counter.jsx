73d13ead46bed532269d8009794bd694078631b6
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-09-04 13:39:43 +0200

    css Counter.jsx and Error404.jsx and add security check Bearer JWT token on swagger.yaml

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -8,11 +8,13 @@ const Counter = () => {
 
   return (
     <MainLayout>
-      <div className="App">
-        <h1>{total}</h1>
-        <button onClick={() => dispatch(increment())}>Increase</button>
-        <button onClick={() => dispatch(reset())}>Reset</button>
-        <button onClick={() => dispatch(decrement())}>Decrease</button>
+      <div className="flex justify-center flex-col mx-2 mt-8 gap-2">
+        <h1 className="flex justify-center">{total}</h1>
+        <div className="flex justify-center mx-2 gap-2 mb-8">
+          <button className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => dispatch(increment())}>Increase</button>
+          <button className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => dispatch(reset())}>Reset</button>
+          <button className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => dispatch(decrement())}>Decrease</button>
+        </div>
       </div>
     </MainLayout>
   )
